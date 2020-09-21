81b15438b25627f80458742cfa4be727b84efd65
Mas Ipin <adm.masipin@gmail.com>
2020-09-21 13:14:03 +0700

    [CLN] some cleanups

--- mt_report/models/report.py
+++ mt_report/models/report.py
@@ -9,9 +9,9 @@ from itertools import groupby
 import logging
 _logger = logging.getLogger(__name__)
 
+
 class Report(models.TransientModel):
     _name = 'mt.reporting'
-
 
     currency_id = fields.Many2one('res.currency', string='Currency', required=True, default=lambda self: self.env.user.company_id.currency_id)
     income = fields.Monetary(string='In-Payment Amount', readonly=True)
@@ -22,21 +22,18 @@ class Report(models.TransientModel):
     dt_to = fields.Date(string='Date To')
     show_report = fields.Boolean('Refresh Report', default=False)
 
-
     @api.one
     def action_show_report(self):
         return True
 
-
     @api.onchange('show_report')
     def _onchange_show_report(self):
         _sql = '''
-            WITH 
+            WITH
                 acc_in AS (SELECT id AS id from account_account WHERE code = 'CR01'),
                 acc_out AS (SELECT id AS id from account_account WHERE code = 'CP01'),
 
                 incoming AS (
-                
                 SELECT 1 as col1, aml.account_id, sum(aml.credit) AS cr
                 FROM account_move_line aml
                 JOIN acc_in ON acc_in.id=aml.account_id
@@ -47,7 +44,6 @@ class Report(models.TransientModel):
                 ),
 
                 outcoming AS (
-               
                 SELECT 1 as col1, aml.account_id, sum(aml.debit) AS dr
                 FROM account_move_line aml
                 JOIN acc_out ON acc_out.id=aml.account_id
@@ -65,11 +61,9 @@ class Report(models.TransientModel):
         outcome = res and res[0].get('outcome', 0.0) or 0.0
         self.income = income
         self.outcome = outcome
-        
-        _sql_tax = _sql.replace('CR01', 'TR01').replace('CP01','TP01')
+        _sql_tax = _sql.replace('CR01', 'TR01').replace('CP01', 'TP01')
         self.env.cr.execute(_sql_tax)
         res = self.env.cr.dictfetchall()
         total_tax = res and res[0].get('outcome', 0.0) or 0.0
         self.total_tax = total_tax
         self.balance = balance - total_tax
-
